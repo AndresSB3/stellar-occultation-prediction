f8e664a8afab1b1c0816dee27438bf86f97fd478
Andrés Sánchez Buitrago <andressanchezbuitrago300000@gmail.com>
2026-07-04 18:29:57 -0500

    Initial implementation of defaulting module

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from reader import update_localdatabase
+from local_config import update_localdatabase, get_config
 
 def main():
-    update_localdatabase()
+    
+    # Option to update
+    update = False
+    
+    # Updating process
+    if update:
+        update_localdatabase()
+        
+    # Get settings
+    settings = get_config()
 
+    
 
 if __name__ == "__main__":
     main()
