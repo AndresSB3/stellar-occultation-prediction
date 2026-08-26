f9ccb89a77239e0f9460d12fe58bd17c34f27065
Andrés Sánchez Buitrago <andressanchezbuitrago300000@gmail.com>
2026-08-26 16:44:03 -0500

    Reinitialized diameter modularization. Implemented paper search

---
+++ tests/test_diameter/url.py
@@ -0,0 +1,33 @@
+import json
+
+
+# Get PDF url from an ADS paper
+def get_pdf_url(paper):
+  
+  # Verifiy links exist and extract links
+  if hasattr(paper, "links_data") and paper.links_data:
+    links = paper.links_data
+    parsed_links = []
+
+    # Parse links
+    for link in links:
+      
+      # Case STR
+      if isinstance(link, str):
+        parsed_links.append(json.loads(link))
+
+      # Case dictionary
+      elif isinstance(link, dict):
+        parsed_links.append(link)
+
+    # Extract PDF link
+    for link in parsed_links:
+      if link.get("type", "").lower() == "pdf":
+        pdf_url = link.get("url")
+        break
+
+  # Default link
+  if not pdf_url:
+    pdf_url = f"https://ui.adsabs.harvard.edu/link_gateway/{paper.bibcode}/PUB_PDF"
+
+  return pdf_url
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,52 @@
-from main import main
+from test_diameter import search
 
+# Mini función para organizar el prompt
+def create_prompt(ids, params):
+  IDs = " OR ".join(ids)
+  Params = " OR ".join(params)
+  return f"({IDs}) AND ({Params}) AND database:astronomy"
+
+# Token de ADS (de cuenta personal)
+token = "dD15JbSYQrvUB5CZXhvnQxqeDfnT0gczHNRycyGu"
+
+# Encabezado para declarar que el pipeline hace pedidos para minería de
+# literatura
+headers = {"User-Agent": "ADS-literature-mining-script"}
+
+# IDs del objeto
+prompt_IDs = ['"(10199) Chariklo"', "Chariklo"]
+
+# Parámetros que se buscan
+prompt_params = [
+  "diameter",
+  "size",
+  "radius",
+  '"effective diameter"',
+  '"equivalent diameter"',
+  "radiometric",
+  '"thermal model"',
+  "occultation",
+  "NEATM",
+  "thermal",
+  "albedo"
+]
+
+# Prompt de búsqueda - Parámetros del objeto e Identificadores del objeto
+prompt = create_prompt(prompt_IDs, prompt_params)
+
+# Campos de extracción
+fields = [
+  "title",
+  "year",
+  "author",
+  "bibcode",
+  "property",
+  "links_data"
+]
+
+# Cantidad máxima de PDFs a buscar (muchos no están disponibles así que se
+# recomienda un valor alto)
+amount = 10
 
 def test_main():
-  assert main()
+  search(token, prompt, fields, headers, amount, verbose=True)
